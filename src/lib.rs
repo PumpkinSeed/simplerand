0237e3cf20f4e784331f683ed53d648236f200fd
PumpkinSeed <qwer.kocka@gmail.com>
2020-06-15 05:59:28 +0000

    Finish implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,28 @@
+#[macro_use]
+extern crate lazy_static;
+
 use std::time::{SystemTime, UNIX_EPOCH};
+use std::sync::Mutex;
 
 const A: i64 = 1103515245;
 const C: i16 = 12345;
 const M: i32 = 1 << 31;
+
+lazy_static! {
+    static ref BASE_RAND: Mutex<Rng> = Mutex::new(Rng::new());
+}
+
+pub fn rand() -> i64 {
+    BASE_RAND.lock().unwrap().rand()
+}
+
+pub fn randn(n: i64) -> i64 {
+    BASE_RAND.lock().unwrap().randn(n)
+}
+
+pub fn rand_range(min: i64, max: i64) -> i64 {
+    BASE_RAND.lock().unwrap().rand_range(min, max)
+}
 
 #[derive(Debug, Copy, Clone)]
 pub struct Rng {
@@ -52,20 +72,64 @@ impl Rng {
 
 #[cfg(test)]
 mod tests {
-    use crate::{Rng};
+    use crate::*;
     use std::time::{Instant};
 
     #[test]
+    fn base_rand() {
+        let n1 = rand();
+        let n2 = rand();
+
+        if n1 == n2 {
+            panic!("{} shouldn't be equal with {}", n1, n2)
+        }
+    }
+
+    #[test]
+    fn base_randn() {
+        let n1 = randn(10000);
+        let n2 = randn(10000);
+
+        if n1 == n2 {
+            panic!("{} shouldn't be equal with {}", n1, n2)
+        }
+        if n1 > 10000 {
+            panic!("{} should be smaller than 10000", n1)
+        }
+        if n2 > 10000 {
+            panic!("{} should be smaller than 10000", n2)
+        }
+    }
+
+    #[test]
+    fn base_rand_range() {
+        let n1 = rand_range(4000, 10000);
+        let n2 = rand_range(4000, 10000);
+
+        if n1 == n2 {
+            panic!("{} shouldn't be equal with {}", n1, n2)
+        }
+        if n1 > 10000 && n1 < 4000 {
+            panic!("{} should be between 4000 and 10000", n1)
+        }
+        if n2 > 10000 && n2 < 4000 {
+            panic!("{} should be between 4000 and 10000", n2)
+        }
+    }
+
+    #[test]
     fn random() {
-        let mut rng = Rng::new();
-        
+        let mut prev_data: i64 = 0;
         for _ in 1..10 {
-            let data = rng.randn(10000);
-            println!("data: {}", data);
+            let data = randn(10000);
+            if prev_data == data {
+                panic!("{} shouldn't be equal with {}", prev_data, data)
+            }
+            prev_data = data
         }   
+        
         let now = Instant::now();    
-        let data = rng.randn(10000);
-        println!("\n\n{:.2?}", now.elapsed());
-        println!("data: {}", data); 
+        let data = randn(10000);
+        println!("Time taken to generate a random number ({}): {:.2?}", data, now.elapsed());
     }
 }
